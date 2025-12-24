89184c53a7208fd0193c8c70cb49579e01edbbf3
SIMO Yann <simoyann2004@gmail.com>
2025-12-24 10:25:44 +0100

    adjusted login

--- Frontend-Mobile/mobileFront/app/login.tsx
+++ Frontend-Mobile/mobileFront/app/login.tsx
@@ -1,4 +1,4 @@
-import { Text, View, StyleSheet, TextInput, Button } from "react-native";
+import { Text, View, StyleSheet, TextInput, Button, Pressable } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Image } from 'expo-image';
 
@@ -11,7 +11,9 @@ export default function Login() {
       </View>
       <TextInput placeholder="Username" style={{height: 40, borderColor: 'gray', borderWidth: 1, marginTop: 20, width: 200, paddingHorizontal: 10}} />
       <TextInput placeholder="Password" secureTextEntry style={{height: 40, borderColor: 'gray', borderWidth: 1, marginTop: 20, width: 200, paddingHorizontal: 10}} />
-      <Button title="Login" onPress={() => { /* Handle login logic here */ }} />
+      <Pressable style={styles.button}>
+        <Text style={{fontSize: 16, color: 'white', backgroundColor: 'blue', paddingVertical: 10, paddingHorizontal: 20, borderRadius: 5}}>Login</Text>
+        </Pressable>
     </SafeAreaView>
   );
 }
@@ -19,4 +21,5 @@ export default function Login() {
 const styles = StyleSheet.create({
   container: { flex: 1, justifyContent: "center", alignItems: "center", backgroundColor: "#ffffffff" },
   image: { width: 150, height: 150 },
+  button: { marginTop: 20 }
 });
